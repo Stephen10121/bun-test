3d47477b55ac92f706542299197d456380990d6b
Stephen Gruzin <stephengrzn@gmail.com>
2023-09-11 21:01:44 -0700

    Testing the http server and paths

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!2");
+import { coolPath, rootPath } from "./paths";
+
+Bun.serve({
+    port: Bun.env.PORT || 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+        console.log(url.pathname);
+
+        if (url.pathname === "/") return rootPath();
+        else if (url.pathname === "/cool" || url.pathname === "/cool/") return coolPath();
+
+        return new Response(null, {status: 404});
+    },
+});
---
+++ paths.ts
@@ -0,0 +1,7 @@
+export function rootPath() {
+    return new Response("Root path");
+}
+
+export function coolPath() {
+    return new Response("Cool path");
+}
